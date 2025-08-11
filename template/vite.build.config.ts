6b41fccf9e7d8ec175b09b53e055ee40d1f24c29
Mikael Arvola <167342266+marvel-uiuc@users.noreply.github.com>
2025-08-11 09:09:00 -0500

    Initial commit

---
+++ template/vite.build.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: "src",
+    build: {
+        outDir: "../dist/cdn",
+        lib: {
+            name: "<%= name %>",
+            entry: "<%= name %>.ts",
+            fileName: "<%= name %>",
+            formats: ["es"],
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === "style.css") return "<%= name %>.css";
+                    return "assets/[name]-[hash][extname]"; // vite default
+                },
+            },
+        },
+    },
+    server: {
+        hmr: false,
+    },
+});
